32ca21bd62fba60cee09403d2946a29cab69bbbf
buk1t <grandee.sculler-2c@icloud.com>
2026-02-22 00:19:12 -0500

    fix commit

--- js/core/config.js
+++ js/core/config.js
@@ -1,6 +1,6 @@
 // js/core/config.js
 export const CONFIG = {
-  WWW_JSON_URL: "api.buk1t.com/public/apps/www/www.json",
+  WWW_JSON_URL: "https://api.buk1t.com/apps/www/www.json",
 
   // UX tuning
   FETCH_TIMEOUT_MS: 5000
